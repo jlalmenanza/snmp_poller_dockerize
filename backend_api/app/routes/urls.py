beb6f1a67ea2c6210dac125d73b0d7190cdac6cf
John Lester Almenanza <jlalmenanza@trends.com.ph>
2021-03-02 11:03:07 +0800

    Added /snmp in routes

--- backend_api/app/routes/urls.py
+++ backend_api/app/routes/urls.py
@@ -13,13 +13,13 @@ from backend_api.app.api.poller_status import PollerStatus
 from backend_api.app.api.networkdiscovery_api import NetworkDiscovery
 
 
-api.add_resource(SnmpPollerApi, '/snmp/poller', '/snmp/poller/<int:id>')
-api.add_resource(PollerService, '/poller/service', '/poller/service/<int:id>', 
-    '/poller/service/<int:id>/logs', '/poller/service/<int:id>/logs/<string:level>')
-api.add_resource(BlacklistApi, '/blacklist')
-api.add_resource(OidApi, '/oid')
-api.add_resource(NetworkDiscovery, '/network/discovery')
-api.add_resource(DataPollingApi, '/data/view/polling')
-api.add_resource(PollerData, '/poller/data', '/poller/data/<string:table_name>')
-api.add_resource(PollerStatus, '/poller/status', '/poller/status/<string:table_name>')
+api.add_resource(SnmpPollerApi, '/snmp/snmp/poller', '/snmp/snmp/poller/<int:id>')
+api.add_resource(PollerService, '/snmp/poller/service', '/snmp/poller/service/<int:id>', 
+    '/snmp/poller/service/<int:id>/logs', '/snmp/poller/service/<int:id>/logs/<string:level>')
+api.add_resource(BlacklistApi, '/snmp/blacklist')
+api.add_resource(OidApi, '/snmp/oid')
+api.add_resource(NetworkDiscovery, '/snmp/network/discovery')
+api.add_resource(DataPollingApi, '/snmp/data/view/polling')
+api.add_resource(PollerData, '/snmp/poller/data', '/snmp/poller/data/<string:table_name>')
+api.add_resource(PollerStatus, '/snmp/poller/status', '/snmp/poller/status/<string:table_name>')
 

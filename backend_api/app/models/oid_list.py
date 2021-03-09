343d337d44f8618c8f77914a2d1d11d36c235676
Vernon Roi S. Sumilang <vssumilang@tgi.com.ph>
2021-03-09 11:03:20 +0800

    modified brand field to device model on oidlist model

--- backend_api/app/models/oid_list.py
+++ backend_api/app/models/oid_list.py
@@ -4,6 +4,6 @@ class OidList(db.Model):
     __tablename__ = 'oid_list'
 
     id = db.Column(db.Integer, primary_key=True)
-    brand_name = db.Column(db.String(50), nullable=False)
+    device_model = db.Column(db.String(50), nullable=False)
     oid_key = db.Column(db.String(50), nullable=False)
     oid = db.Column(db.String(255), nullable=False)
